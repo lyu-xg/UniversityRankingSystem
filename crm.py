a31a9308b2d1dcf96509a65b833b9bc4bff6c039
iyidgnaw <iyidgnaw@outlook.com>
2016-12-09 22:36:12 -0500

    12.9 5th commit

--- crm.py
+++ crm.py
@@ -3,28 +3,24 @@ __author__ = 'wangdiyi'
 
 import numpy as np
 import random
-import sys
+
 
 RElATION_SIZE = 5
 FEATURE_SIZE = 5
 WEIGHT_SIZE = FEATURE_SIZE
 
-LEARNING_RATE = 0.001
-LAMBDA = 0.001
-CONFS = {} # Dictionary for relative vectors, key is the name of conference
-             # RElATION_SIZE size is 5 for each.
-SCHOOLS = {} # Dictionary for relative vectors, key is the name of schools name.
-             # RElATION_SIZE size is 5 for each.
+LEARNING_RATE = 0.01
+LAMBDA = 0.01
 TRAIN = []
-# VALID = []
 TEST = []
+VALID = []
 WEIGHT_VECTORS = {}
 
 
 
 def pre_data():
     # randomly init CONFS and SCHOOLS
-    school_list = ["CMU","NEU", "NYU","UCB","UCI","BUPT"]
+    school_list = range(100)
     conf_list = ["KDD","ICDM","CVPR","AAAI","ICDL","ICML"]
     # [[school_name, conference_name], [f1, f2, f3, f4], X]
 
@@ -32,18 +28,10 @@ def pre_data():
         school = school_list[random.randint(0, len(school_list)-1)]
         conf = conf_list[random.randint(0, len(conf_list)-1)]
         features = np.random.rand(4)
-        X = np.random.random_integers(20)
+        X = np.random.random_integers(10)
         data = [[school,conf],features,X]
-        if i <= 800:
-            TRAIN.append(data)
-        else:
-            TEST.append(data)
-    for school in school_list:
-        s = np.random.randn(RElATION_SIZE) * 0.5
-        SCHOOLS[school] = s
-    for conf in conf_list:
-        c = np.random.randn(RElATION_SIZE) * 0.5
-        CONFS[conf] = c
+        TRAIN.append(data)
+
     for school in school_list:
         for conf in conf_list:
             WEIGHT_VECTORS[(school,conf)] = np.random.randn(WEIGHT_SIZE) * 0.5
@@ -52,74 +40,67 @@ def pre_data():
 def train():
     # [[school_name,conference_name],[f1,f2,f3,f4],X]
     global WEIGHT_VECTORS
-    global CONFS
-    global SCHOOLS
+
 
     total_diff = 0.0
     for data in TRAIN:
         real_hit = data[2]* 1.0
-        school_vector = SCHOOLS[data[0][0]]
-        conf_vector = CONFS[data[0][1]]
         weight = WEIGHT_VECTORS[tuple(data[0])]
-        f0 = np.dot (school_vector,conf_vector)
+        f0 = 1
         feature_vector = np.array([f0])
         feature_vector = np.concatenate((feature_vector,data[1]),axis = 0)
         predict_hit = np.dot(feature_vector,weight)
         diff = real_hit - predict_hit
-        total_diff += diff
+        total_diff += abs(diff)
 
-        #calculate delta for w c_j s_i
-        ds = 2 * diff * weight[0] * conf_vector
-        dc = 2 * diff * weight[0] * school_vector
+        # calculate delta for w c_j s_i
         dw = 2 * diff * feature_vector
 
-        #update
-        SCHOOLS[data[0][0]] += LEARNING_RATE*(ds + LAMBDA*school_vector)
-        CONFS[data[0][1]] += LEARNING_RATE*(dc + LAMBDA*conf_vector)
+        # update
         WEIGHT_VECTORS[tuple(data[0])] += LEARNING_RATE*(dw + LAMBDA*weight)
 
-    return total_diff
+    return total_diff/800
 
 
 def test():
     # [[school_name,conference_name],[f1,f2,f3,f4],X]
 
-    global WEIGHT_VECTORS
-    global CONFS
-    global SCHOOLS
-
     total_diff = 0.0
     for data in TEST:
-
         real_hit = data[2] * 1.0
-        school_vector = SCHOOLS[data[0][0]]
-        conf_vector = CONFS[data[0][1]]
         weight = WEIGHT_VECTORS[tuple(data[0])]
-        f0 = np.dot(school_vector, conf_vector)
+        f0 = 1
         feature_vector = np.array([f0])
         feature_vector = np.concatenate((feature_vector, data[1]), axis=0)
         predict_hit = np.dot(feature_vector, weight)
         diff = real_hit - predict_hit
-        total_diff += diff
-    print "total error on test:%f" % total_diff
+        total_diff += abs(diff)
+    print "total error on test:%f" % (total_diff/200)
 
 
-# def valid():
-#
-#     global WEIGHT_VECTOR
-#
-#     total_diff = 0.0
-#     for data in VALID:
-#         real_hit = data[2] * 1.0
-#         school_vector = SCHOOLS[data[0][0]]
-#         conf_vector = CONFS[data[0][1]]
-#         f0 = np.dot(school_vector, conf_vector)
-#         feature_vector = np.zeros((1, FEATURE_SIZE))
-#         predict_hit = np.dot(feature_vector, WEIGHT_VECTOR)
-#         diff = real_hit - predict_hit
-#         total_diff += diff
-#     print "Current Error in sum:"
-#     print total_diff
+def valid(error):
+
+    global WEIGHT_VECTORS
+    global LEARNING_RATE
+
+    total_diff = 0.0
+    for data in VALID:
+        real_hit = data[2] * 1.0
+        weight = WEIGHT_VECTORS[tuple(data[0])]
+        f0 = 1
+        feature_vector = np.array([f0])
+        feature_vector = np.concatenate((feature_vector, data[1]), axis=0)
+        predict_hit = np.dot(feature_vector, weight)
+        diff = real_hit - predict_hit
+        total_diff += abs(diff)
+    avg_error = (total_diff / 100)
+    print "total error on valid:%f" % avg_error
+    if avg_error>error:
+        LEARNING_RATE *= 1.5
+    else:
+        LEARNING_RATE *= 0.5
+    return error
+
 
 
 # def evaluate():
@@ -128,25 +109,27 @@ def test():
 
 def learn():
 
-    # f_handler = open('h40.txt', 'a')
-    # sys.stdout = f_handler
 
     for iter in range(300):
         print "Iter %d" % iter
         print "Training..."
         loss = train()
-        print "total error on train:%f"%loss
+        print "total error on train:%f" % loss
+        # error = valid(pasterror)
+        # pasterror = error
+        #print "Testing"
+        #test()
 
-        print "Testing"
-        test()
-        #f_handler.close()
+    return
 
-
-def main():
-
+def basic_info():
     print "LEARNING_RATE = %f" % LEARNING_RATE
     print "LAMBDA = %f" % LAMBDA
+    return
+
+def main():
     pre_data()
+    basic_info()
     learn()
 
 if __name__ == '__main__':
